e6d48b0500cfcf1c5d6ceff00a538aeafb0ccbef
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-03-07 22:06:55 +0530

    UI Screen Updated

--- src/screens/UIWorks.jsx
+++ src/screens/UIWorks.jsx
@@ -1,12 +1,11 @@
-import React , {useState}from 'react'
-import {AnimatePresence, delay, motion} from "framer-motion"
+import React , {useState} from 'react'
+import {AnimatePresence, motion} from "framer-motion"
 import {BsDot} from "react-icons/bs"
 import room1 from "../assests/room1.jpg"
 import room2 from "../assests/room2.jpg"
 import room3 from "../assests/room3.jpg"
 
 const UIWorks = () => {
-   
    const slide = [
        {
         url :room1
@@ -18,31 +17,25 @@ const UIWorks = () => {
         url :room3
        },
    ]
-
   const [currentIndex, setCurrentIndex] = useState(0)
-
-
   const goToSlide = (slideIndex)=>{
      setCurrentIndex(slideIndex)
   }
-
   return (
-      <div className='sm:w-full sm:p-10 sm:h-[90vh] flex flex-col lg:flex-row items-center justify-center border p-10'>
+      <motion.div exit={{backgroundColor: "#666666", y:-80, opacity:0.2}} transition={{duration:1}}  className='sm:w-full sm:p-10 sm:h-[90vh] flex flex-col lg:flex-row items-center justify-center border p-10'>
           <div className='w-full h-72  lg:w-1/2 sm:h-[80vh] lg:h-full flex flex-col justify-center items-center p-4  '>
-            {/* <AiOutlineLeft className='cursor-pointer mx-4 text-gray-400' onClick={prevSlide}/> */}
             <div className='w-32 sm:w-60 lg:w-64  rounded-2xl overflow-hidden' style={{boxShadow:"rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"}}>
               <AnimatePresence>
               <motion.img 
                  key={currentIndex}
                  initial={{scale:0, opacity:0}}
-                 animate={{once:"true",scale:0.9, opacity:1}}
+                 whileInView={{scale:0.9, opacity:1}}
                  transition={{duration:1}}
-                 exit={{scale:0.5, opacity:0, delay:0, duration:0}}
+                 exit={{scale:0.5, opacity:0}}
                 src={slide[currentIndex].url}  className='w-32 sm:w-60 lg:w-64 overflow-hidden'>
                 </motion.img>
               </AnimatePresence>
             </div>
-            {/* <AiOutlineRight className='cursor-pointer mx-4 text-gray-400'onClick={nextSlide}/> */}
             <div className='w-32 h-5 flex items-center justify-center mt-2 cursor-pointer'>
                  {
                   slide.map((slide, index)=>{
@@ -68,8 +61,7 @@ const UIWorks = () => {
                 </motion.p>
             </div> 
            </div>
-      </div>
+      </motion.div>
   )
 }
-
 export default UIWorks
