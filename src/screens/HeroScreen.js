ed909a4e0b82567825824b3daf61a1e2e849bf4c
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-03-10 22:30:17 +0530

    10 march UI updated

--- src/screens/HeroScreen.js
+++ src/screens/HeroScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {AnimatePresence, motion, useAnimation, useScroll, useTransform, useMotionValue} from "framer-motion"
+import React, { useEffect, useState } from 'react'
+import {AnimatePresence, motion,} from "framer-motion"
 import me1 from "../assests/me1.png"
 import Footer from './FooterScreen'
 import { useRef } from "react";
@@ -8,9 +8,12 @@ import NavbarComp from './NavbarComp';
 import room1 from "../assests/room1.jpg"
 import room2 from "../assests/room2.jpg"
 import room3 from "../assests/room3.jpg"
+import boy from "../assests/boy.jpg"
+import popup from "../assests/popup.mp3"
 
 
 function Section({ children }) {
+
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
   
@@ -50,34 +53,75 @@ const slide = [
 ]
 
 const HeroScreen = () => {
-    let  ref  = useRef()
-    let {scrollYProgress} = useScroll();
-    let opacity = useTransform(scrollYProgress, [1,0],[0, 1])
+  const [trigger, setTrigger] = useState(false)
+  const [value, setValue] = useState(true)
+
+   
+  const play = ()=>{
+     new Audio(popup).play(value)
+     value(false)
+  }
+
+    useEffect(()=>{
+       setTimeout(()=>{
+          setTrigger(true)
+       },2000)
+    },[])
 
   return (
     <>
     <NavbarComp />
-    <motion.div ref={ref} exit={{backgroundColor: "#666666", y:-80, opacity:0.2}} transition={{duration:1}} className='w-full h-auto flex flex-col items-center justify-center'>
+    <motion.div exit={{backgroundColor: "#666666", y:-80, opacity:1}} transition={{duration:1.2}} className='w-full h-auto flex flex-col items-center justify-center'>
           <motion.div  className="flex flex-row  justify-center items-center rounded-full  mt-4 p-4  border  shadow-lg shadow-gray-500/60  ">
           <motion.img  initial={{opacity:0, y:-100}} animate={{y:0, opacity:1}} transition={{ duration:2, type:"keyframes", stiffness:120}}  src={me1} alt="me1" className=" w-52 sm:w-72  h-full rounded-full border border-gray-200  hover:shadow-2xl"/>
             </motion.div>
 
         <motion.div 
                  initial={{y:60,opacity:0,}}
-                 animate={{ once:"true", y:10, opacity:1 }}
+                 animate={{ once:"true", y:10, opacity:1,}}
                  transition={{delay:1, duration:1}}
                  whileInView={{textShadow: "8.0px 16.0px 16.0px hsl(0deg 0% 0% / 0.25)"}}
-                  className='w-full h-full text-black py-6 '>
+                 className='w-full h-full text-black p-8 relative'
+                 >
+
+    {/* BOY POP UP */}
+                <AnimatePresence>
+                {
+                  trigger ? (
+                    <motion.div
+                       initial={{x:-250,}}
+                       animate={{x:-10,}}
+                       transition={{delay:1, duration:1}}
+                       exit={{x:-250, delay:0, duration:0}}
+                       className='absolute hidden  left-0 w-60 h-auto lg:flex flex-row items-center justify-between -bottom-2'>
+                        <motion.img 
+                          initial={{x:-120}}
+                          animate={{x:-10, rotate:[0,20]}}
+                          transition={{delay:2, duration:5, type:"spring", stiffness:300,damping:20}}
+                          src={boy} className='w-28 self-start relative -top-10  '
+                        />
+                        <div className='w-1/2 '>
+                          <h1 className='text-xl font-opensans'>Hey!! There</h1>  
+                          <h1 className='text-sm font-opensans'>Do Contact me</h1>  
+                          <button className='w-2 h-2 rounded-full text-slate-700 absolute -top-10 right-0 mx-3 cursor-pointer'onClick={()=>{setTrigger(false); play()}}>X</button>
+                        </div>      
+                    </motion.div>
+                  
+                  ) : null
+                }
+               </AnimatePresence>
+     {/* BOY POP UP */}
+
                  <h1 className='text-center text-sm md:text-3xl font-playfair text-gray-500'>Hi! I am Sumit Sahni a UI designer</h1>
-               <motion.div  style={{opacity}} className='w-full mx-auto sm:1/2 md:w-4/5 lg:w-3/5 xl:w-4/5'>
+               <motion.div  className='w-full mx-auto sm:1/2 md:w-4/5 lg:w-3/5 xl:w-4/5'>
                  <h2 className='p-2 text-center text-3xl md:text-6xl font-playfair font-semibold text-gray-700'>Transforming ideas into interactive digital Realities</h2>  
                </motion.div>
         </motion.div>
 
         <motion.div
-                 initial={{opacity:0}} animate={{y:0,opacity:1}} transition={{duration:1 ,delay:2}}
-                 whileHover={{shadow: "8.0px 16.0px 16.0px hsl(0deg 0% 0% / 0.25)"}}
-                  className='w-[90%] mx-auto  sm:w-1/2 md:w-1/3 sm:mx-auto mt-10  text-center p-4 border border-1 hover:border-double  hover:shadow-2xl delay-550 hover:bg-gray-500 hover:text-white transition duration-700 ease-in-out mb-16 '>
+                 initial={{opacity:0}} whileInView={{y:0,opacity:1}} viewport={{once:'true'}} transition={{duration:1 ,delay:2}}
+                  whileHover={{shadow: "8.0px 16.0px 16.0px hsl(0deg 0% 0% / 0.25)"}}
+                  className='w-[90%] mx-auto  sm:w-1/2 md:w-1/3 sm:mx-auto mt-10  text-center p-4 border border-1 hover:border-double  delay-550 hover:bg-gray-500 hover:text-white transition duration-700 ease-in-out mb-16 '>
                <h1>
                     <span className='w-96 '>
                     <p>Currently at Pune Maharashtra studying Master's in Computer Application, set to post graduate in May 2023.</p></span>
@@ -104,7 +148,7 @@ const HeroScreen = () => {
       
          {/* EXPERTISE */}
          
-          <section className=' w-full flex items-center '>
+          <section className=' w-full flex items-center p-12 '>
                 <div className='w-full p-1  sm:h-auto md:h-auto md:w-[80%] mx-auto   flex flex-row flex-wrap items-center justify-center  gap-4 '>
                       {
                         slide.map(( item, i)=>{
@@ -113,7 +157,7 @@ const HeroScreen = () => {
                               initial={{opacity:0, x:i%2 ===0?-50:100,y:50}}
                               whileInView={{opacity:1, x:0, y:0}}
                               viewport={{once:'true'}}
-                              transition={{duration:0.4, delay:i*0.2}}
+                              transition={{duration:0.5, delay:i*0.2}}
                              
                               className='w-28 h-28 sm:w-52 sm:h-52 md:w-80 md:h-60 bg-cyan-600 flex items-center justify-center  shadow-[rgba(0, 0, 0, 0.25) 0px 25px 50px -12px]'>
                                 <h1 className='text-white text-3xl font-extralight text-center '>React Js</h1>
--- src/screens/UIWorks.js
+++ src/screens/UIWorks.js
@@ -51,10 +51,10 @@ const UIWorks = () => {
   return (
     <>
     <NavbarComp/>
-      <motion.div initial={{y:0, opacity:0}} animate={{ y:0, opacity:1}} transition={{duration:1.3}} exit={{backgroundColor: "#666666", y:-100, opacity:0.2}} className='sm:w-full p-4 sm:h-auto flex flex-col lg:flex-row items-center justify-center sm:p-10'>
+      <motion.div  transition={{duration:1.3}} exit={{backgroundColor: "#666666", y:-100, opacity:0.2}} className='sm:w-full p-4 sm:h-auto flex flex-col lg:flex-row items-center justify-center sm:p-10'>
           <div className=' w-full h-72  lg:w-1/2 sm:h-auto lg:h-full flex flex-col justify-center items-center '>
             <motion.div 
-               initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1}} transition={{duration:1}}
+               initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1}} transition={{duration:1, delay:1}}
             className='w-32 sm:w-60 lg:w-64 rounded-2xl overflow-hidden' style={{boxShadow:"rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"}}>
                  <motion.img 
                    initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1 , delay:1}}
@@ -77,12 +77,12 @@ const UIWorks = () => {
                <motion.h1 
                 initial={{x:400, scale:0}}
                 animate={{x:0, scale:1}}
-                transition={{duration:1.2}}
+                transition={{duration:1.2, delay:1}}
                className='text-3xl text-center sm:text-6xl w-full font-opensans font-extralight text-gray-600 lg:text-left'>React Native Project</motion.h1>
                <motion.p 
                 initial={{y:100}}
                 animate={{y:0}}
-                transition={{duration:1.2}}
+                transition={{duration:1.2,  delay:1}}
                 className= "leading-8 mt-4 text-center sm:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nam reiciendis! Aspernatur commodi vel excepturi, repudiandae sint praesentium rerum iste laborum dignissimos, vitae tenetur reprehenderit hic quia saepe iure. Odio!
                 </motion.p>
                 <button className='hover:bg-gray-600 hover:border-2 hover:text-white bg-gray-400 text-white font-bold py-2 px-4 rounded-full mt-4'>
@@ -93,24 +93,25 @@ const UIWorks = () => {
       </motion.div>
 
                                 {/* / REACT OTHER PROJECT  */}
-       <section   className='flex flex-col items-center '>
+       <section  className='flex flex-col items-center border-2 '>
              <div 
                  className='w-full p-8 mt-12'>
                 <motion.h1 
                  initial={{scale:0}}
                  whileInView={{scale:1}}
                  viewport={{once:'true'}}
-                 transition={{duration:1.2,}}
+                 transition={{duration:1.2,  type: "spring", stiffness: 400, damping: 10 }}
                 className='text-center text-5xl sm:text-6xl sm:p-8 font-opensans font-extralight text-gray-600'>From Coding to Creation</motion.h1>
              </div>
-             <div className='w-full h-[60vh]  sm:h-auto  flex flex-col md:flex-row item-center justify-between p-4 sm:p-16'>
+
+             <div className='w-full h-[100vh]  sm:h-auto  flex flex-col md:flex-row item-center justify-between p-4 sm:p-16'>
                  
                <motion.div
                  initial={{opacity:0}}
                  whileInView={{opacity:1}}
                  viewport={{once:'true'}}
                  transition={{duration:1.3}}
-                 className="transform transition duration-300 ease-out cursor-pointer sm:w-[85%] max-w-full md:w-[96%] lg:w-2/5  h-[70%] hover:h-[80%] xl:w-2/3  md:h-[25rem] lg:h-[40vh] mx-auto flex flex-col md:flex-row lg:flex-row items-center justify-centerbg-white rounded-2xl  hover:text-white hover:font-bold hover:bg-gradient-to-r from-green-400 to-blue border-2">
+                 className="transform transition duration-300 ease-out cursor-pointer sm:w-[85%] max-w-full md:w-[96%] lg:w-2/5  h-[70%]  xl:w-2/3  md:h-[25rem] lg:h-[40vh] mx-auto flex flex-col md:flex-row lg:flex-row items-center justify-centerbg-white rounded-2xl  hover:text-white hover:font-bold hover:bg-gradient-to-r from-green-400 to-blue border-2">
                          <div className='h-96 lg:w-12 hidden  md:hidden   lg:flex flex-col items-center justify-center'>
                             <p className='rotate-180 ' style={{"writingMode":"vertical-rl"}}>UX Design + Research  (by Sumit)</p>
                          </div>
@@ -139,7 +140,7 @@ const UIWorks = () => {
                            whileInView={{opacity:1}}
                            viewport={{once:'true'}}
                            transition={{duration:1.3}}
-              className=" mt-4 transform transition duration-300 ease-out cursor-pointer w-full sm:w-[85%] md:w-[96%] lg:w-2/5 xl:w-2/3 h-[70%] hover:h-[80%] md:h-[25rem] lg:h-[40vh] mx-auto flex flex-col md:flex-row lg:flex-row items-center justify-center   bg-white rounded-2xl  hover:text-white hover:font-bold hover:bg-gradient-to-r from-green-400 to-blue border-2">
+              className=" mt-4 sm:mt-0 transform transition duration-300 hover:ease-out cursor-pointer w-full sm:w-[85%] md:w-[96%] lg:w-2/5 xl:w-2/3 h-[70%]  md:h-[25rem] lg:h-[40vh] mx-auto flex flex-col md:flex-row lg:flex-row items-center justify-center   bg-white rounded-2xl  hover:text-white hover:font-bold hover:bg-gradient-to-r from-green-400 to-blue border-2">
                          <div className=' h-96 lg:w-12 hidden  md:hidden   lg:flex flex-col items-center justify-center'>
                             <p className='rotate-180 ' style={{"writingMode":"vertical-rl"}}>UX Design + Research  (by Sumit)</p>
                          </div>
@@ -162,6 +163,8 @@ const UIWorks = () => {
                            </div>
                         </div>
               </motion.div>
+             
+
              
                {/* jhcvxzcb */}
              </div>
