348256b31b3ef2eafa84550796d444d99032e681
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-04-29 04:01:20 +0530

    New Added

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -33,7 +33,7 @@ const Main = () => {
       <NavbarComp/>
      <section className='h-auto md:h-[385vh] bg-[#0a192f] absolute '>
         {/* MY INTRO  */}
-         <div className='sm:h-[70vh] md:h-[100vh] w-full sm:w-2/3 mx-auto py-16 px-2 md:px-16 mt-24'>
+         <div className='h-[80vh] sm:h-[60vh] w-full sm:w-2/3 mx-auto py-16 px-2 md:px-16 mt-24'>
             <div>
                 <h1 className='px-1 py-4 text-[#64ffda] mx-2 sm:py-6 sm:px-2 font-semibold'>Hi, my name is</h1>
             </div>
@@ -55,8 +55,7 @@ const Main = () => {
          </div>
 
            {/* ABOUT ME  */}
-          <div className='sm:h-auto w-full sm:w-3/4 mx-auto py-16 sm:py-0  px-2 sm:px-16'>
-
+          <div className='sm:h-auto w-full md:w-3/4 mx-auto sm:py-10 md:py-16   px-2 sm:px-16'>
              <div className='px-4 sm:px-3 flex flex-row items-center gap-6 mb-5 lg:mb-10'>
                 <h1 className='md:text-4xl text-white font-semibold font-comfort'><span className='text-[#64ffda]'>0.1 </span>About Me</h1>
                 <div className='w-48 sm:w-96 border border-gray-600  sm:mt-1'></div>
@@ -94,7 +93,7 @@ const Main = () => {
                     <div>
                   </div>
 
-                  <div className='w-full sm:w-[80%] mx-auto lg:w-[50%] flex flex-col items-center lg:items-end py-10 '>
+                  <div className='w-full  md:w-[80%] mx-auto lg:w-[50%] flex flex-col items-center lg:items-end py-10 '>
                      <div className="relative flex flex-row  justify-center items-center rounded-full  mt-4 p-2   border-2 border-[#64ffda]  shadow-lg hover:border-dotted  ">
                        <img src={me1} alt="me1" className=" w-52 sm:w-72  h-full rounded-full border hover:opacity-75  opacity-40 cursor-pointer"/>
                      </div>                  
@@ -104,7 +103,7 @@ const Main = () => {
           
 
            {/* ACHIVEMENT & EXPERIENCE  */}
-           <div className='h-auto md:h-[60vh] w-full sm:w-3/4 mx-auto  px-2 sm:px-24'> 
+           <div className='h-auto md:h-[60vh] w-full md:w-3/4 mx-auto  px-2 sm:px-24'> 
               <div className='px-4 sm:px-3 flex flex-row items-center gap-6 mb-5 lg:mb-20'>
                  <h1 className='md:text-4xl text-white font-semibold font-comfort'><span className='text-[#64ffda]'>0.2 </span>Achivement & Experience</h1>
                  <div className='w-48 sm:w-96 border border-gray-600  sm:mt-1'></div>
@@ -129,7 +128,7 @@ const Main = () => {
                   </div>
                </div>
             
-                 <div className='w-full py-12 sm:py-0 sm:w-[80%] mx-auto lg:w-[70%] flex flex-col px-4 md:px-10 '>
+                 <div className='w-full py-12 md:py-0 md:w-[80%] mx-auto lg:w-[70%] flex flex-col px-4 md:px-10 '>
                                {
                                 (toggle?
                                  <motion.div
@@ -194,7 +193,7 @@ const Main = () => {
 
            {/* GET IN TOUCH  */}
 
-          <div className='h-[60vh] sm:h-[60vh] w-full sm:w-3/4 mx-auto sm:px-24 md:mt-10 lg:mt-32 '> 
+          <div className='h-[60vh] sm:h-[40vh] w-full sm:w-full md:w-3/4 mx-auto sm:px-24 sm:mt-32 lg:mt-32'> 
               <div className='w-full md:w-3/4 mx-auto flex flex-col items-center justify-center'>
                   <h1 className='px-1 py-4 text-[#64ffda] mx-2 sm:py-6 sm:px-2 font-semibold'><span>0.3 What's Next?</span></h1>
                   <div>
